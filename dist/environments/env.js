5539ba8aa9b60aabe6b84c993b823967f0c81170
victorGonzalezMZ <victor309710gonz@gmail.com>
2021-05-07 13:40:07 -0500

    Cambios

--- dist/environments/env.js
+++ dist/environments/env.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     API: {
-        PORT: 3003
+        PORT: 3000
     },
     MONGODB: {
         HOST: 'localhost',
